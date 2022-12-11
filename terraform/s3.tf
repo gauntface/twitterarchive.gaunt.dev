b87176f3186537d4f3b916f559cf707214143452
Matt Gaunt-Seo <matt@gaunt.dev>
2022-12-11 13:10:13 -0800

    Lets try this out

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,13 +7,3 @@ resource "aws_s3_bucket_acl" "twitterarchive-bucket-acl" {
   bucket = aws_s3_bucket.twitterarchive-bucket.id
   acl    = "private"
 }
-
-resource "aws_s3_bucket_object" "short-term-cache" {
-  for_each = fileset("../public/", "*.{html,xml,json}")
-  bucket = aws_s3_bucket.twitterarchive-bucket.id
-  key = each.value
-  source = "../public/${each.value}"
-  etag = filemd5("../public/${each.value}")
-  acl = "public-read"
-  cache_control = "max-age=86400"
-}
