60fdd354775f87bbb0b98e51b1f311b8d42864b1
Matt Gaunt-Seo <matt@gaunt.dev>
2022-12-12 10:00:22 -0800

    Fix up the scripts

--- themes/twitterarchive/assets/js/always-async.ts
+++ themes/twitterarchive/assets/js/always-async.ts
@@ -2,7 +2,7 @@ import {init} from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
 
 init({
-	dsn: "https://74547611df4c4a0097edec0053db428f@o1296550.ingest.sentry.io/6623002",
+	dsn: "https://748ea69ae17d48088e7047f414d9055b@o1296550.ingest.sentry.io/4504317324820480",
 	integrations: [new BrowserTracing()],
 	tracesSampleRate: 0.4,
 });
